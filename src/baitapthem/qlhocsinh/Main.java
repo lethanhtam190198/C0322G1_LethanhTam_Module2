42a2703e187bb5e3d9fc882f2866f7446f9a6f87
lethanhtam190198 <tamle011998@gmail.com>
2022-05-16 08:08:12 +0700

    bt

--- src/baitapthem/qlhocsinh/Main.java
+++ src/baitapthem/qlhocsinh/Main.java
@@ -1,8 +1,5 @@
 package baitapthem.qlhocsinh;
 
-
-import crud.Product;
-
 import java.util.Scanner;
 
 public class Main {
@@ -11,93 +8,119 @@ public class Main {
     static Scanner scanner = new Scanner(System.in);
 
     static {
-        listPerson[0] = new Student(1,"abc",20,false,10);
-        listPerson[1] = new Teacher(2,"bbb",22,true,10000);
-        count = 2;
+        listPerson[0] = new Student(1, "abc", 20, false, 10);
+        listPerson[1] = new Teacher(4, "bbbd", 22, true, 10000);
+        listPerson[2] = new Teacher(5, "de", 22, true, 10000);
+        count = 3;
     }
+
     public static void display() {
         for (Person item : listPerson) {
             if (item != null && item instanceof Student) {
                 System.out.println((Student) item);
             }
-            if((item != null && item instanceof Teacher)){
+            if ((item != null && item instanceof Teacher)) {
                 System.out.println((Teacher) item);
             }
         }
     }
-    public  static void add(){
-        do{
+
+    public static void add() {
+        do {
             System.out.println("1.Add Student " +
                     "\n2.Add Teacher");
-            int choose= Integer.parseInt(scanner.nextLine());
-            switch (choose){
+            int choose = Integer.parseInt(scanner.nextLine());
+            switch (choose) {
                 case 1:
                     System.out.println("nhap id");
-                    int id=Integer.parseInt(scanner.nextLine());
+                    int id = Integer.parseInt(scanner.nextLine());
                     System.out.println("nhap ten");
-                    String name= scanner.nextLine();
+                    String name = scanner.nextLine();
                     System.out.println("nhap tuoi");
-                    int age=Integer.parseInt(scanner.nextLine());
+                    int age = Integer.parseInt(scanner.nextLine());
                     //System.out.println("nhap gioi tinh:");
-                    boolean gender=false;
+                    boolean gender = false;
                     System.out.println("nhap diem");
-                    double point=Double.parseDouble(scanner.nextLine());
-                    Student student=new Student(id,name,age,true,point);
-                    listPerson[count]=(Student) student;
+                    double point = Double.parseDouble(scanner.nextLine());
+                    Student student = new Student(id, name, age, true, point);
+                    listPerson[count] = (Student) student;
                     count++;
                     display();
                     break;
                 case 2:
                     System.out.println("nhap id");
-                    int id1=Integer.parseInt(scanner.nextLine());
+                    int id1 = Integer.parseInt(scanner.nextLine());
                     System.out.println("nhap ten");
-                    String name1= scanner.nextLine();
+                    String name1 = scanner.nextLine();
                     System.out.println("nhap tuoi");
-                    int age1=Integer.parseInt(scanner.nextLine());
+                    int age1 = Integer.parseInt(scanner.nextLine());
                     //System.out.println("nhap gioi tinh:");
-                    boolean gender1=true;
+                    boolean gender1 = true;
                     System.out.println("nhap Luong");
-                    double salary=Double.parseDouble(scanner.nextLine());
-                    Teacher teacher=new Teacher(id1,name1,age1,true,salary);
-                    listPerson[count]=(Person) teacher;
+                    double salary = Double.parseDouble(scanner.nextLine());
+                    Teacher teacher = new Teacher(id1, name1, age1, true, salary);
+                    listPerson[count] = (Person) teacher;
                     count++;
                     display();
                     break;
             }
-        }while (false);
+        } while (false);
     }
-    public static void search(){
-        boolean flag=true;
-        do{
+
+    public static void search() {
+        boolean flag = true;
+        do {
             System.out.println("1.Tim theo Id:" +
                     "\n2.Tim theo ten:");
-            int choose= Integer.parseInt(scanner.nextLine());
-            switch (choose){
+            int choose = Integer.parseInt(scanner.nextLine());
+            switch (choose) {
                 case 1:
                     System.out.println("nhap id muon tim kiem");
                     int id = Integer.parseInt(scanner.nextLine());
                     for (Person item : listPerson) {
                         if (item != null && item.getId() == id) {
                             System.out.println(item);
-                            flag=false;
+                            flag = false;
                         }
                     }
-                    if(flag) {
+                    if (flag) {
                         System.out.println("khong tim thay!");
 
                     }
                     break;
                 case 2:
                     System.out.println("nhap tu muon tim kiem:");
-
+                    String search = scanner.nextLine();
+                    for (int i = 0; i < listPerson.length; i++) {
+                        if (listPerson[i] != null && listPerson[i].getName().contains(search)) {
+                            System.out.println(listPerson[i]);
+                        }
+                        else {
+                            System.out.println("khong tim thay");
+                            break;
+                        }
+                    }
 
             }
-        }while (false);
+        } while (false);
+    }
+
+    public static void delete() {
+        System.out.println("nhap id muon xoa:");
+        int id = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < listPerson.length - 1; i++) {
+            if (listPerson[i] != null && id == listPerson[i].getId()) {
+                for (int j = i; j < listPerson.length && listPerson[j] != null; j++) {
+                    listPerson[j] = listPerson[j + 1];
+                }
+            }
+
+        }
+        display();
     }
 
     public static void main(String[] args) {
         do {
-
             System.out.println("----------PerSon-----------");
             System.out.println("1.Display");
             System.out.println("2.Add");
@@ -115,14 +138,14 @@ public class Main {
                     System.out.println("Add");
                     add();
                     break;
+                case 3:
+                    System.out.println("Delete");
+                    delete();
+                    break;
                 case 4:
                     System.out.println("Search");
                     search();
                     break;
-//                case 4:
-//                    System.out.println("Delete");
-//                    delete();
-//                    break;
                 case 5:
                     System.exit(0);
             }
@@ -130,6 +153,3 @@ public class Main {
         while (true);
     }
 }
-
-//    }
-//}
